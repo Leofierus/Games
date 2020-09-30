d14c4d9a1235fd11da662c3ac8908b6dca710fb0
Malhar Patel <leofierus@gmail.com>
2020-09-30 17:06:22 +0530

    Update main.cpp
    Fixed minor issues, feel free to fix the bugs too

--- 05 Minesweeper/main.cpp
+++ 05 Minesweeper/main.cpp
@@ -19,32 +19,18 @@ int main()
     for (int i=1;i<=10;i++)
      for (int j=1;j<=10;j++)
       {
-        sgrid[i][j]=10;
-        if (rand()%5==0)  grid[i][j]=9;
-        else grid[i][j]=0;
-      }
-
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
-      {
         int n=0;
         if (grid[i][j]==9) continue;
         if (grid[i+1][j]==9) n++;
         if (grid[i][j+1]==9) n++;
         if (grid[i-1][j]==9) n++;
         if (grid[i][j-1]==9) n++;
-        if (grid[i+1][j+1]==9) n++;
-        if (grid[i-1][j-1]==9) n++;
-        if (grid[i-1][j+1]==9) n++;
-        if (grid[i+1][j-1]==9) n++;
         grid[i][j]=n;
       }
 
     while (app.isOpen())
     {
         Vector2i pos = Mouse::getPosition(app);
-        int x = pos.x/w;
-        int y = pos.y/w;
 
         Event e;
         while (app.pollEvent(e))
@@ -63,7 +49,6 @@ int main()
          for (int j=1;j<=10;j++)
           {
            if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
-           s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
            s.setPosition(i*w, j*w);
            app.draw(s);
           }
