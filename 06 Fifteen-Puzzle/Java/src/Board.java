f1147eb3b3f7787e8680350176a074686ddf3be3
Dhruv Topiwala <dhruv.m.topiwala@gmail.com>
2020-10-01 09:51:45 +0530

    Multilevel Mode for FifteenPuzzle Java

--- 06 Fifteen-Puzzle/Java/src/Board.java
+++ 06 Fifteen-Puzzle/Java/src/Board.java
@@ -1,65 +1,88 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
 public class Board extends JPanel {
     BufferedImage img;
+    int level;
     int curPos;
-    Board(BufferedImage img){
-        setPreferredSize(new Dimension(img.getHeight(),img.getWidth()));
-        setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
-        setBackground(Color.BLACK);
-        this.img=img;
-        setLayout(new GridLayout(3,3));
+
+    Board(BufferedImage img, int level) {
+        this.setPreferredSize(new Dimension(img.getHeight(), img.getWidth()));
+        this.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
+        this.setBackground(Color.BLACK);
+        this.img = img;
+        this.level = level;
+        this.setLayout(new GridLayout(level, level));
     }
-    void add(){
-        Cell[] cells= ImageSpliter.Splitter(img);
-        for(Cell cell:cells){
-            add(cell);
+
+    void add() {
+        Cell[] cells = ImageSpliter.Splitter(this.img, this.level);
+        Cell[] var2 = cells;
+        int var3 = cells.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            Cell cell = var2[var4];
+            this.add(cell);
         }
-        curPos=3*3-1;
+
+        this.curPos = this.level * this.level - 1;
     }
-    boolean check(){
-        for(int i=0;i<8;i++){
-            Cell cell=(Cell)getComponent(i);
-            if(i!=cell.solPos)
+
+    boolean check() {
+        for(int i = 0; i < this.level * this.level - 1; ++i) {
+            Cell cell = (Cell)this.getComponent(i);
+            if (i != cell.solPos) {
                 return false;
+            }
         }
+
         return true;
     }
-    void shuffle(){
-        for(int i=0;i<100;i++){
-            int random=(int)Math.round(Math.random()*3);
-            switch(random){
-                case 0:swap(MoveCell.up(curPos));break;
-                case 1:swap(MoveCell.down(curPos));break;
-                case 2:swap(MoveCell.left(curPos));break;
-                case 3:swap(MoveCell.right(curPos));break;
+
+    void shuffle() {
+        for(int i = 0; i < 100; ++i) {
+            int random = (int)Math.round(Math.random() * 3.0D);
+            switch(random) {
+                case 0:
+                    this.swap(MoveCell.up(this.curPos, this.level));
+                    break;
+                case 1:
+                    this.swap(MoveCell.down(this.curPos, this.level));
+                    break;
+                case 2:
+                    this.swap(MoveCell.left(this.curPos, this.level));
+                    break;
+                case 3:
+                    this.swap(MoveCell.right(this.curPos, this.level));
             }
         }
 
     }
-    void swap(int newPos){
-        if(newPos==-1){
-            return;
+
+    void swap(int newPos) {
+        if (newPos != -1) {
+            int firstIndex;
+            int secondIndex;
+            if (newPos > this.curPos) {
+                firstIndex = this.curPos;
+                secondIndex = newPos;
+            } else {
+                firstIndex = newPos;
+                secondIndex = this.curPos;
+            }
+
+            Component first = this.getComponent(firstIndex);
+            Component second = this.getComponent(secondIndex);
+            this.remove(first);
+            this.remove(second);
+            this.add(second, firstIndex);
+            this.add(first, secondIndex);
+            this.curPos = newPos;
         }
-        int firstIndex;
-        int secondIndex;
-        if(newPos>curPos){
-            firstIndex=curPos;
-            secondIndex=newPos;
-        }
-        else{
-            firstIndex=newPos;
-            secondIndex=curPos;
-        }
-        Component first=getComponent(firstIndex);
-        Component second=getComponent(secondIndex);
-        remove(first);
-        remove(second);
-        add(second,firstIndex);
-        add(first,secondIndex);
-        curPos=newPos;
     }
-
 }
-
--- 06 Fifteen-Puzzle/Java/src/ImageSpliter.java
+++ 06 Fifteen-Puzzle/Java/src/ImageSpliter.java
@@ -1,30 +1,38 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 
 public class ImageSpliter {
-    static Cell[] Splitter(BufferedImage img){
-        final int figureWidth, figureHeight;
-        int x=0,y=0,count=0;
-        figureWidth = img.getWidth()/3;
-        figureHeight = img.getHeight()/3;
-        Cell[] cells= new Cell[9];
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(i == 2 && j == 2){
-                    cells[count]=new Cell(null,count);
+    public ImageSpliter() {
+    }
+
+    static Cell[] Splitter(BufferedImage img, int level) {
+        int x = 0;
+        int y = 0;
+        int count = 0;
+        int figureWidth = img.getWidth() / level;
+        int figureHeight = img.getHeight() / level;
+        Cell[] cells = new Cell[level * level];
+
+        for(int i = 0; i < level; ++i) {
+            for(int j = 0; j < level; ++j) {
+                if (i == level - 1 && j == level - 1) {
+                    cells[count] = new Cell((ImageIcon)null, count);
                     cells[count].setPreferredSize(new Dimension(figureWidth, figureHeight));
                     cells[count].setBackground(Color.BLACK);
-                    continue;
+                } else {
+                    cells[count] = new Cell(new ImageIcon(img.getSubimage(x, y, figureWidth, figureHeight)), count);
+                    cells[count].setPreferredSize(new Dimension(figureWidth, figureHeight));
+                    x += figureWidth;
+                    ++count;
                 }
-                cells[count]=new Cell(new ImageIcon(img.getSubimage(x, y, figureWidth, figureHeight)),count);
-                cells[count].setPreferredSize(new Dimension(figureWidth, figureHeight));
-                x += figureWidth;
-                count++;
             }
+
             x = 0;
             y += figureHeight;
         }
+
         return cells;
     }
 }
